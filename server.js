7d9d079e8f7522757f6dc4c819a1a382dd48b627
Charles A. Silva <charles_araujo@ymail.com>
2018-11-22 17:06:01 +0000

    First Commit

---
+++ server.js
@@ -0,0 +1,34 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+var pathPage = function(page){
+      return __dirname + "/" + page + ".html";
+};
+
+var router = function(pathname){
+    if(pathname && pathname != "/"){
+        var exist = fileExists( pathPage(pathname) );
+        return exist ? pathPage(pathname) : pathPage("erro");
+    }
+    return pathPage("index");
+};
+
+var fileExists = function(filePath){
+    try{
+        return fs.statSync(filePath).isFile();
+    }catch (err){
+        return false;
+    }
+};
+
+var server = http.createServer(function (request, response) {
+    var page = router( url.parse(request.url).pathname );
+    fs.readFile(page, function(err, data){
+        response.end(data);
+    });
+});
+
+server.listen(3000, function () {
+    console.log('Servidor rodando na porta 3000');
+});
